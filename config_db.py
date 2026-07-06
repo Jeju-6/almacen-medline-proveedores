41488657d289f29f69a2f16d74dcd646b5eb0724
Jesus Sanchez <yisus.s566.ys.ys@gmail.com>
2026-07-06 16:48:44 -0500

    Agregar gitignore y remover archivos sensibles

--- config_db.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    'host': 'aws-1-us-east-2.pooler.supabase.com',
-    'port': 5432,
-    'database': 'postgres',
-    'user': 'postgres.alvxqmzaiocvmdkjgpqh',
-    'password': 'AlmacenMedline'
-}
-
-DATABASE_URL = f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
